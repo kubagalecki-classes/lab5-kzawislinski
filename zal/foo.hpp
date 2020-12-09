6594a6d7589815245248dc7fbbdcfc4269d824e3
kzawislinski <73909365+kzawislinski@users.noreply.github.com>
2020-12-09 23:25:29 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,25 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
+    std::vector< char > piesel;
+
+    auto it = people.rbegin();
+    
+    for(it; it!=people.rend(); ++it){
+        it->birthday();
+        
+        if(it->isMonster())
+        {
+            piesel.push_back('n');
+        }
+        else
+        {
+           piesel.push_back('y');
+        }
+        
+    };
+    return piesel;
+
+
     return {};
 }
